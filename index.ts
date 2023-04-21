29f64d5f7ae984c514a4f8864d939622fc91dfd3
Vincent <vincent@medanosol.es>
2023-04-21 12:33:43 +0100

    Export types and mongoose instance

--- index.ts
+++ index.ts
@@ -1,6 +1,19 @@
+import mongoose from "mongoose";
+
 export { MongoDB } from "./src/dal/mongodb/MongoDB";
+
+export { mongoose };
 
 export { BLL } from "./src/bll/BLL";
 
 export { User } from "./src/dal/user/User";
 export { Session } from "./src/dal/session/Session";
+
+//TYPES
+
+export { MainRepository } from "./src/dal/mongodb/MainRepository";
+export { GenericDocumentClass } from "./src/dal/mongodb/GenericDocumentClass";
+export { GenericDocumentType } from "./src/dal/mongodb/interfaces/Generics";
+export { GenericModelType } from "./src/dal/mongodb/interfaces/Generics";
+export { GenericSubDocumentType } from "./src/dal/mongodb/interfaces/Generics";
+export { GenericSubDocumentArrayType } from "./src/dal/mongodb/interfaces/Generics";
